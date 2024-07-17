def639e2b1f98b7bd2016ab99b492cc6a0b212c7
jamesqquick <james.q.quick@gmail.com>
2024-07-17 15:20:18 -0500

    only query content for the logged in user

--- app/components/ReviewTweets.tsx
+++ app/components/ReviewTweets.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import prisma from '../lib/prisma';
 import EditTweet from './EditTweet';
+import { auth } from '@clerk/nextjs/server';
 
 export default async function ReviewTweets() {
+  const { userId } = auth().protect();
   const tweets = await prisma.tweet.findMany({
     where: {
       approved: false,
+      user_id: userId,
     },
   });
 
